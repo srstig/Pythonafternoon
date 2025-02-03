450948f5b1881748aa6ea5b006d35fce9a74015b
srstig <rayogutu36@gmail.com>
2025-02-03 16:07:26 +0300

    monday class

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display an output
+
+print("Ray")
+print("emobilis")
+print(100)
+print(10+20)
---
+++ variable.py
@@ -0,0 +1,18 @@
+#variable is a container used to store a value
+location = "westlands"
+firstname= "Ray"
+lastname = "Ogutu"
+school = "eMobilis"
+x = 39
+y = 2
+
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname, lastname)
+print("My full name is", firstname,lastname)
+print(school, "is a coding school")
+print(x+y)
+print("The product of", x,"and", y, "is", x*y)
