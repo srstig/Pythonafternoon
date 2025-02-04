61c998de35255c674a5fb29bd7088b81ad1ca85e
srstig <rayogutu36@gmail.com>
2025-02-04 16:14:39 +0300

    tuesday

---
+++ datatype.py
@@ -0,0 +1,29 @@
+number = 50 #Int
+num = 56.98 #Float
+greeting = "hello there" #Str
+hasfeathers = True #Boolean
+
+#Data structures
+cars = ["Mercedes", "Toyota", "Ferrari"] #List
+fruits = ("Banana","Orange","Apple") #Tuple
+countries = {"England", "France", "Germany"} #Set
+student = {
+    "firstname": "Ann",
+    "course": "MIT",
+    "age": 19,
+    "nationality" : "Kenyan"
+}
+
+print(number)
+print(num)
+print(greeting)
+print(hasfeathers)
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student ["nationality"])
+
+#Typecasting -converting one data type to another
+print(float(number))
+print(int(num))
---
+++ string.py
@@ -0,0 +1,21 @@
+text = "Hello there"
+course = "MIT"
+
+print(text)
+print(course)
+
+#Accessing an element in a string
+print(text[0])
+print(text[1])
+
+#size of a string
+print(len(text))
+
+#Modifying a string
+print(text.upper())
+print(course.lower())
+
+#String Concatenation
+print(text + course)
+
+print(text + " " +course)
